435cdcaabd80e5aedcc2cf1b141a886e581c8dd0
Jona Bako <87908322+jonabako@users.noreply.github.com>
2021-07-24 18:48:42 +0200

    Add files via upload

---
+++ C/a1/a1_p3.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main() {
+	float result; /* The result of the division */
+	int a = 5;
+	float b = 13.5;
+	result = a / b;
+	printf("The result is %f\n", result);
+	return 0;
+}
