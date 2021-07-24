bf9413094fa6bad0c29d3c7ae947a77c2cb07795
Jona Bako <87908322+jonabako@users.noreply.github.com>
2021-07-24 18:51:27 +0200

    Delete C/a1 directory

--- C/a1/a1_p2.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-int main() {
-	int result; /* The result of our calculation */
-	result = (2 + 7) * 9 / 3;
-	printf("The result is %d\n", result);
-	return 0;
-}
-
--- C/a1/a1_p3.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-int main() {
-	float result; /* The result of the division */
-	int a = 5;
-	float b = 13.5;
-	result = a / b;
-	printf("The result is %f\n", result);
-	return 0;
-}
