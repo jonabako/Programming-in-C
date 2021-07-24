922f1ea5eb829aa7929e911c43235a9dbc364aff
Jona Bako <87908322+jonabako@users.noreply.github.com>
2021-07-24 19:55:23 +0200

    Add files via upload

---
+++ a8/a8_p4/stack.h
@@ -0,0 +1,21 @@
+/*
+CH-230-A
+stack.h
+Jona Bako
+jbako@jacobs-university.de
+*/
+#ifndef stack_h
+#define stack_h
+#include <stdlib.h>
+#include <stdio.h>
+struct stack {
+    unsigned int count;
+    int array[12]; // Container
+};
+struct stack* pop(struct stack* list);
+void push(int item, struct stack* list);
+void isFull(struct stack* list);
+void isEmpty(struct stack* list);
+void empty(struct stack* list);
+void printstack(struct stack* my_list);
+#endif //
